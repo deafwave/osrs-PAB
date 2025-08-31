94bc25c5422bf39f74171c627619609242fd041c
Chandler Ferry <chandlerferry@gmail.com>
2025-08-30 21:23:48 -0700

    chore: jewelry

---
+++ routines/modules/jewelry.r
@@ -0,0 +1,17 @@
+#Digsite Pendant
+ACQUIRE:11194,4-32
+
+# Games Necklace
+ACQUIRE:3853,4-32
+
+# Ring of Dueling
+ACQUIRE:2552,4-32
+
+# Necklace of Passage
+ACQUIRE:21146,4-32
+
+# Ring of returning
+ACQUIRE:21129,2-8
+
+# Burning amulet
+ACQUIRE:21166,2-8
