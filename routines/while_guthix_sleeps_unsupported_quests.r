9aa7115710664a8c399cd2d9627d7dd5b20373aa
Chandler Ferry <chandlerferry@gmail.com>
2025-08-30 21:24:03 -0700

    chore: kinda useful?

---
+++ routines/while_guthix_sleeps_unsupported_quests.r
@@ -0,0 +1,102 @@
+# While Guthix Sleeps Quest Chain Requirements
+# Requirements for quests leading up to While Guthix Sleeps that aren't supported
+
+# A Tail of Two Cats - Requirements (Prerequisites: Icthlarin's Little Helper, Gertrude's Cat)
+ACQUIRE:Catspeak amulet,1
+ACQUIRE:Death rune,5
+ACQUIRE:Chocolate cake,1
+ACQUIRE:Logs,1
+ACQUIRE:Tinderbox,1
+ACQUIRE:Bucket of milk,1
+ACQUIRE:Shears,1
+ACQUIRE:Rake,1
+ACQUIRE:Seed dibber,1
+ACQUIRE:Potato seed,4
+ACQUIRE:Vial of water,1
+
+# Tears of Guthix - Requirements (Prerequisite: 43 Quest Points)
+SKILL:FIREMAKING:49
+SKILL:CRAFTING:20
+SKILL:MINING:20
+ACQUIRE:Sapphire,1
+ACQUIRE:Bullseye lantern,1
+ACQUIRE:Swamp tar,1
+ACQUIRE:Chisel,1
+ACQUIRE:Tinderbox,1
+ACQUIRE:Pickaxe,1
+ACQUIRE:Rope,1
+
+# Eadgar's Ruse - Requirements (Prerequisites: Druidic Ritual, Troll Stronghold, Death Plateau)
+SKILL:HERBLORE:31
+ACQUIRE:Climbing boots,1
+ACQUIRE:Vodka,1
+ACQUIRE:Pineapple chunks,1
+ACQUIRE:Logs,1
+ACQUIRE:Grain,10
+ACQUIRE:Raw chicken,5
+ACQUIRE:Pestle and mortar,1
+ACQUIRE:Ranarr potion (unf),1
+
+# Dream Mentor - Requirements (Prerequisites: Lunar Diplomacy, Eadgar's Ruse)
+# NOTE: Requires 85 Combat Level - handled by combat stats elsewhere
+ACQUIRE:Seal of passage,1
+ACQUIRE:Goutweed,1
+ACQUIRE:Astral rune,1
+ACQUIRE:Tinderbox,1
+ACQUIRE:Hammer,1
+ACQUIRE:Pestle and mortar,1
+
+# The Path of Glouphrie - Requirements (Prerequisites: Eyes of Glouphrie, Grand Tree, Waterfall Quest, Tree Gnome Village)
+SKILL:STRENGTH:60
+SKILL:SLAYER:56
+SKILL:THIEVING:56
+SKILL:RANGED:47
+SKILL:AGILITY:45
+ACQUIRE:Mith grapple,1
+ACQUIRE:Crossbow,1
+
+# Garden of Tranquillity - Requirements (Prerequisites: Creature of Fenkenstrain)
+SKILL:FARMING:25
+ACQUIRE:Ring of charos,1
+ACQUIRE:Rake,1
+ACQUIRE:Seed dibber,1
+ACQUIRE:Spade,1
+ACQUIRE:Secateurs,1
+ACQUIRE:Watering can,1
+ACQUIRE:Gardening trowel,1
+ACQUIRE:Plant cure,2
+ACQUIRE:Marigold seed,2
+ACQUIRE:Cabbage seed,6
+ACQUIRE:Onion seed,6
+ACQUIRE:Hammer,1
+ACQUIRE:Rune essence,1
+ACQUIRE:Pestle and mortar,1
+ACQUIRE:Filled plant pot,1
+ACQUIRE:Ultracompost,2
+
+# Below Ice Mountain - Requirements (Prerequisite: 16 Quest Points)
+SKILL:MINING:10
+ACQUIRE:Cooked meat,1
+ACQUIRE:Bread,1
+ACQUIRE:Knife,1
+ACQUIRE:Beer,1
+
+# Defender of Varrock - Requirements (Multiple prerequisites including Garden of Tranquillity, What Lies Below, etc.)
+SKILL:SMITHING:55
+SKILL:HUNTER:52
+
+# While Guthix Sleeps - Final Requirements (180 Quest Points + all above prerequisites)
+SKILL:THIEVING:72
+SKILL:MAGIC:67
+SKILL:AGILITY:66
+SKILL:FARMING:65
+SKILL:HERBLORE:65
+SKILL:HUNTER:62
+# NOTE: Attack + Strength >= 130 for Warriors' Guild access
+ACQUIRE:Ring of charos(a),1
+ACQUIRE:Seed dibber,1
+ACQUIRE:Lantern lens,1
+ACQUIRE:Unpowered orb,1
+ACQUIRE:Bronze med helm,1
+ACQUIRE:Iron chainbody,1
+ACQUIRE:Sapphire lantern,1
